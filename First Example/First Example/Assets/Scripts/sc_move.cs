b79f9e0f15cef24714d29a34542f06e770d0f03b
panxogol <juan.martinez.p@usach.cl>
2020-04-14 19:47:40 -0400

    Adding coins, points, enemies and a winning point

--- First Example/First Example/Assets/Scripts/sc_move.cs
+++ First Example/First Example/Assets/Scripts/sc_move.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Threading;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Collections.Specialized;
+//using System.Threading;
+using System;
 using UnityEngine;
 
 public class sc_move : MonoBehaviour
 {
     public float forceValue;
     public float jumpValue;
+    public float zJumpLimit;
+    public float yJumpMin;
+    int points = 0;
+    int deaths = 0;
+    int wins = 0;
+    int totalCoins;
     private new Rigidbody rigidbody;
-    private new AudioSource jumpSound;
+    private AudioSource jumpSound;
+    private AudioSource coinSound;
+    private AudioSource deathSound;
+    private AudioSource winingSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        jumpSound = GetComponent<AudioSource>();
+        AudioSource[] sphereSounds = GetComponents<AudioSource>();
+        jumpSound = sphereSounds[0];
+        coinSound = sphereSounds[1];
+        deathSound = sphereSounds[2];
+        winingSound = sphereSounds[3];
+
+        //contar monedas iniciales}
+        GameObject[] coinsTotal = GameObject.FindGameObjectsWithTag("coin");
+        totalCoins = coinsTotal.Length;
+    }
+
+    //Texto de Puntaje
+    void OnGUI()
+    {
+        GUI.Label(new Rect(5, 5, 200, 20), "Points: " + points.ToString());
+        GUI.Label(new Rect(5, 25, 200, 20), "Deaths: " + deaths.ToString());
+        GUI.Label(new Rect(5, 45, 200, 20), "Winnings: " + wins.ToString());
     }
 
     // Update is called once per frame
@@ -31,6 +57,23 @@ public class sc_move : MonoBehaviour
             rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
             jumpSound.Play();
         }
+        
+        //Para dispositivos moviles
+        if(Input.touchCount == 1)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began && Mathf.Abs(rigidbody.velocity.y) < yJumpMin)
+            {
+                rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+                jumpSound.Play();
+            }
+        }
+
+        //Utilizando acelerometro en Z
+        if (Input.acceleration.z > zJumpLimit && Mathf.Abs(rigidbody.velocity.y) < yJumpMin)
+        {
+            rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+            jumpSound.Play();
+        }
     }
 
     void FixedUpdate()
@@ -38,5 +81,35 @@ public class sc_move : MonoBehaviour
         rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"),
                                        0,
                                        Input.GetAxis("Vertical")) * forceValue);
+
+/*#if UNITY_ANDROID
+        rigidbody.AddForce(new Vector3(Input.acceleration.x,
+                                       0,
+                                       Input.acceleration.y) * forceValue);
+#endif */
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.tag == "coin")
+        {
+            points++;
+            coinSound.Play();
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "enemy")
+        {
+            deaths++;
+            deathSound.Play();
+            rigidbody.transform.position = new Vector3(0, 2, 0);
+        }
+
+        if (collision.gameObject.tag == "finish" && points == totalCoins)
+        {
+            wins++;
+            winingSound.Play();
+            rigidbody.transform.position = new Vector3(0, 6, 0);
+        }
     }
 }
---
+++ First Example/android proyect/Assets/Scripts/cameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class cameraController : MonoBehaviour
+{
+	void OnGUI()
+	{
+		GUI.Label(new Rect(5, 5, 200, 20), "Accelerometer X: " + Input.acceleration.x);
+		GUI.Label(new Rect(5, 25, 200, 20), "Accelerometer Y:" + Input.acceleration.y);
+		GUI.Label(new Rect(5, 45, 200, 20), "Accelerometer Z:" + Input.acceleration.z);
+		GUI.Label(new Rect(5, 100, 200, 20), "Number of touches: " + Input.touchCount);
+		for (int i = 0; i < Input.touchCount; i++)
+		{
+			PrintTouchData(Input.touches[i]);
+		}
+	}
+
+	void PrintTouchData(Touch t)
+	{
+		GUI.Label(new Rect(t.position.x, Screen.height - t.position.y, 100, 20), t.position.ToString());
+		GUI.Label(new Rect(t.position.x, Screen.height - t.position.y + 20, 100, 20), "Finger: " + t.fingerId.ToString());
+		GUI.Label(new Rect(t.position.x, Screen.height - t.position.y + 40, 100, 20), "Tap count: " + t.tapCount.ToString());
+	}
+}
